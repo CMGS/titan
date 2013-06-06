d92ac1115b2cdd9ad4736997650338baa7aacf01
CMGS <ilskdw@gmail.com>
2013-06-06 15:21:33 +0800

    add git model

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # encoding: UTF-8
 
+from .git import init_git_db
 from .account import init_account_db
 from .organization import init_organization_db
 
 def init_db(app):
+    init_git_db(app)
     init_account_db(app)
     init_organization_db(app)
 
