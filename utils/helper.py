e4520367500c9bdf8c9a87f5d477900dc1a7afbb
CMGS <ilskdw@gmail.com>
2013-06-08 12:07:51 +0800

    cause flask didnt merge pr, so I modify utils.helper.MethodView

--- utils/helper.py
+++ utils/helper.py
@@ -12,6 +12,24 @@ class MethodView(_):
     def render_template(self, **kwargs):
         return render_template(self.filename, **kwargs)
 
+    @classmethod
+    def as_view(cls, name, *class_args, **class_kwargs):
+        def view(*args, **kwargs):
+            return view.instance.dispatch_request(*args, **kwargs)
+
+        if cls.decorators:
+            view.__name__ = name
+            view.__module__ = cls.__module__
+            for decorator in cls.decorators:
+                view = decorator(view)
+
+        view.instance = cls(*class_args, **class_kwargs)
+        view.__name__ = name
+        view.__doc__ = cls.__doc__
+        view.__module__ = cls.__module__
+        view.methods = cls.methods
+        return view
+
 def make_view(module_name):
     def generate_view_func(cls, **kwargs):
         module = kwargs.get('module', None) or module_name
