dd4f2452a80ba80231271a575a7bb8835303e97b
CMGS <ilskdw@gmail.com>
2013-06-18 15:57:01 +0800

    stash

--- views/repos/repos.py
+++ views/repos/repos.py
@@ -7,12 +7,17 @@ from utils.helper import MethodView
 from utils.account import login_required
 from utils.organization import member_required
 
+from query.organization import get_teams_by_ogranization
+
 logger = logging.getLogger(__name__)
 
 class Create(MethodView):
     decorators = [member_required(admin=False), login_required('account.login')]
     def get(self, organization, members):
-        pass
+        teams = get_teams_by_ogranization(organization.id)
+        return self.render_template(teams=teams)
 
     def post(self, organization, members):
-        pass
+        name = request.form.get('name', None)
+        path = request.form.get('path', None)
+
