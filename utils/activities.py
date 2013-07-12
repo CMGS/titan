114df3a17ad1cf116229adac45d863b92b1617b8
CMGS <ilskdw@gmail.com>
2013-07-12 15:51:45 +0800

    use request cache

--- utils/activities.py
+++ utils/activities.py
@@ -1,12 +1,15 @@
 #!/usr/local/bin/python2.7
 #coding:utf-8
 
+from sheep.api.local import reqcache
+
 from query.repos import get_repo
 from query.organization import get_team
 from query.account import get_user, get_user_from_alias
 from utils.repos import format_time, format_branch, get_url
 
-cache={}
+cache=reqcache
+
 def render_push_action(action, organization, team=None, repo=None):
     repo = repo or cache.get(action['repo_id'], None) or get_repo(action['repo_id'])
     repo_url = cache.get('%d_url' % action['repo_id'])
@@ -22,19 +25,19 @@ def render_push_action(action, organization, team=None, repo=None):
         repo_url = get_url('repos.view', organization, repo)
         branch_url = get_url('repos.view', organization, repo, version=action['branch'])
 
-    cache[action['repo_id']] = repo
-    cache['%s_url' % action['repo_id']] = repo_url
-    cache['%d_branch_url' % action['repo_id']] = branch_url
+    cache.set(action['repo_id'], repo)
+    cache.set('%s_url' % action['repo_id'], repo_url)
+    cache.set('%d_branch_url' % action['repo_id'], branch_url)
     action['repo'] = repo
     action['repo_url'] = repo_url
     action['branch_url'] = branch_url
     action['committer'] = get_user(action['committer_id'])
     for i in xrange(0, len(action['data'])):
         log = action['data'][i]
-        author = cache.get(log['author_email'], None)
-        if not author:
+        author = cache.get(log['author_email'], False)
+        if author is False:
             author = get_user_from_alias(log['author_email'])
-            cache[log['author_email']] = author
+            cache.set(log['author_email'], author)
         log['author'] = author
         log['author_time'] = format_time(log['author_time'])
         log['message'] = log['message'].decode('utf8')
